4a9d7aef86c7e39d9bc74a4bc2806503214a22e4
hed854 <hed854k@gmail.com>
2018-12-31 00:55:00 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Infra
+
+## AWS
+
+Requirements: 
+
+* IAM programmatic user with appropriate permissions
+* AWS credential file 
+* Terraform
+
+Plan: `terraform plan -var-file=variables.tfvars`
+
+
---
+++ aws/main.tf
@@ -0,0 +1,56 @@
+variable "credentials_file" {}
+variable "credntials_profile" {}
+variable "region" {}
+variable "public_key" {}
+variable "project_name" {}
+
+provider "aws" {
+	region = "${var.region}"
+	shared_credentials_file = "${var.credentials_file}"
+	profile = "${var.credentials_profile}"
+}
+
+resource "aws_key_pair" "terraform_key" {
+	key_name = "terraform_key"
+	public_key = "${var.public_key}" 
+}
+
+# Create a random EC2 ubuntu machine
+#resource "aws_instance" "random" {
+#	ami = "ami-00035f41c82244dab"
+#	instance_type = "t1.micro"
+#	key_name = "terraform_key"
+#}
+
+resource aws_db_instance "postgres" {
+	# And not "postgresql"!! see https://github.com/terraform-providers/terraform-provider-aws/issues/4492
+	engine = "postgres"
+	allocated_storage = 20
+	instance_class = "db.t2.small"
+	name = "${var.project_name}"
+	username = "${var.project_user}"
+	password = "${var.project_password}"
+}
+
+resource "aws_security_group_rule" "allow_ssh" {
+	type = "ingress"
+	from_port = 22
+	to_port = 22
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "sg-2db30c5c"
+}
+
+resource "aws_security_group_rule" "allow_postgres" {
+	type = "ingress"
+	from_port = 5432
+	to_port = 5432
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "sg-2db30c5c"
+}
+
+output "instances_host" {
+	value = ["${aws_db_instance.postgres.*.endpoint}"]
+}
+
